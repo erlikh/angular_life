190329bca0fad28b7b8864c22a8fa88cdcb2b0f1
Nick Erlikh <n.erlikh@gmail.com>
2013-06-06 18:46:00 +0700

    Add main loop starting and stopping

--- life.js
+++ life.js
@@ -9,16 +9,20 @@ angular.module("life", [])
         size: "@size"
       },
       template: "" +
-        "<div ng-repeat='row in matrix' class='matrix'>" +
-         "<div ng-repeat='cell in row'>" +
-            "<div ng-class='{active: cell.is_active}' class='cell' ng-click='cell_clicked(cell)'></div>" +
+        "<div>" +
+          "<div ng-repeat='row in matrix' class='matrix'>" +
+           "<div ng-repeat='cell in row'>" +
+              "<div ng-class='{active: cell.is_active}' ng-click='cell_clicked(cell)' class='cell'></div>" +
+            "</div>" +
           "</div>" +
+          "<div ng-click='start(true)'>Start</div>" +
+          "<div ng-click='stop()'>Stop</div>" +
         "</div>",
       link: function(){},
       controller: function($scope){
-        var seed_matrix;
+        var seed_matrix, stopped=false;
 
-        seed_matrix = function(size) {
+        seed_matrix = function(size){
           var matrix = [];
           for(var row=0; row<size; row++){
             var current_line = [];
@@ -28,13 +32,25 @@ angular.module("life", [])
             matrix[row] = current_line;
           }
           return matrix;
-        }
-
-        $scope.matrix = seed_matrix(10);
+        };
 
         $scope.cell_clicked = function(cell){
           cell.is_active = !cell.is_active;
         }
+
+        $scope.start = function(forced){
+          if(forced){ stopped = false; }
+          if(!stopped){
+            setTimeout(function(){ $scope.start() }, 100);
+          }
+        };
+
+        $scope.stop = function(){
+          stopped = true;
+        }
+
+        $scope.matrix = seed_matrix(10);
+        $scope.start();
       }
     }
   });
