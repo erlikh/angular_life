cf243a0b706ac43df40bae3cd69da91beccca351
Nick Erlikh <n.erlikh@gmail.com>
2013-06-06 16:15:55 +0700

    Add simple directive

--- life.js
+++ life.js
@@ -1,1 +1,10 @@
-console.log("It works!");
+angular.module("life", [])
+  .directive("game", function factory(){
+    return {
+      restrict: "AEC",
+      replace: true,
+      template: "<div>It works</div>",
+      transclude: true,
+      link: function(){}
+    }
+  });
