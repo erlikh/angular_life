3dd40b802498acf32d9da5a821f5c7fb2bc0462e
Nick Erlikh <n.erlikh@gmail.com>
2013-06-07 03:13:38 +0700

    Fix neighbors checking

--- life.js
+++ life.js
@@ -11,16 +11,17 @@ angular.module("life", [])
       template: "" +
         "<div>" +
           "<div ng-repeat='row in matrix' class='matrix'>" +
-           "<div ng-repeat='cell in row'>" +
+            "<div ng-repeat='cell in row'>" +
               "<div ng-class='{active: cell.is_active}' ng-click='cell_clicked(cell)' class='cell'></div>" +
             "</div>" +
+            "<br/>" +
           "</div>" +
           "<div ng-click='start(true)'>Start</div>" +
           "<div ng-click='stop()'>Stop</div>" +
         "</div>",
       link: function(){},
       controller: function($scope){
-        var check_element, map_matrix, seed_matrix, stopped=true, size=10; //TODO: Remove size variable.
+        var stopped=true, size=10; //TODO: Remove size variable.
 
         var get_active_neighbors = function(matrix, index){
           var elem_row = index[0],
@@ -31,36 +32,47 @@ angular.module("life", [])
             if(!matrix[row]){ continue; }
 
             for(var column=elem_column-1; column<=elem_column+1; column++){
-              if(row === elem_column && column === elem_column){ continue; }
+              if(row === elem_row && column === elem_column){ continue; }
               var neighbor = matrix[row][column];
               if(!neighbor || !neighbor.is_active){ continue;}
-
               neighbors_count++
             }
           }
           return neighbors_count;
         };
 
-        check_element = function(element, index){
-          var is_active = Math.random() * 10 > 5;
+        var check_element = function(element, index){
+          var is_active = false;
           var active_neighbors_count = get_active_neighbors($scope.matrix, index);
 
+          if(element.is_active){
+            if(active_neighbors_count===2 || active_neighbors_count===3){
+              is_active = true;
+            }
+          } else if(active_neighbors_count === 3) {
+            is_active = true;
+          }
           return {is_active: is_active};
         };
 
-        map_matrix = function(matrix, callback){
-          for(var row=0; row<size; row++){
-            var current_line = matrix[row] || []; //TODO: Remove default value. Used only for seeding.
-            for(var column=0; column<size; column++){
-              current_line[column] = callback(current_line[column], [row, column], matrix);
-            }
-            matrix[row] = current_line;
-          }
-          return matrix;
+        var map_matrix = function(matrix, callback){
+          var new_matrix = matrix.map(function(vector, row){
+            return vector.map(function(element, column){
+              return callback(element, [row, column], matrix);
+            });
+          });
+          return new_matrix;
         };
 
-        seed_matrix = function(){
-          return map_matrix([], function(){ return {is_active: false}; });
+        var seed_matrix = function(){
+          var matrix = [];
+          for(var row=0; row < size; row++){
+            matrix[row] = [];
+            for(var column=0; column<size; column++){
+              matrix[row][column] = [];
+            }
+          }
+          return map_matrix(matrix, function(){ return {is_active: false}; });
         };
 
         $scope.cell_clicked = function(cell){
@@ -71,10 +83,7 @@ angular.module("life", [])
           if(forced){ stopped = false; }
           if(stopped){ return; }
 
-          var new_matrix = $scope.matrix;
-          map_matrix(new_matrix, check_element);
-
-
+          $scope.matrix = map_matrix($scope.matrix, check_element);
           $timeout(function(){ $scope.start() }, 1000); //TODO: Make it clean why don't window.timeout re-render the screen.
         };
 
