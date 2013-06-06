6c47d144b239e370f0bcaa658d7c0f2e0d5cc013
Nick Erlikh <n.erlikh@gmail.com>
2013-06-06 18:15:01 +0700

    Add matrix drawing

--- life.js
+++ life.js
@@ -3,19 +3,31 @@
 angular.module("life", [])
   .directive("game", function factory(){
     return {
-      restrict: "AEC",
+      restrict: "E",
       replace: true,
-      scope: false,
+      scope: {
+        size: "@size"
+      },
       template: "" +
-        "<div ng-repeat='cell in cells'>" +
-          "<div ng-class='{active: cell.is_active}' class='cell' ng-click='cell_clicked(cell)'></div>" +
+        "<div ng-repeat='row in matrix' class='matrix'>" +
+         "<div ng-repeat='cell in row'>" +
+            "<div ng-class='{active: cell.is_active}' class='cell' ng-click='cell_clicked(cell)'></div>" +
+          "</div>" +
         "</div>",
       transclude: false,
       link: function(){
 
       },
-      controller: function($scope){
-        $scope.cells = [{is_active: true}, {is_active: false}]
+      controller: function($scope, $element){
+        $scope.matrix = [];
+        var size = 10;
+        for(var row=0; row<size; row++){
+          var current_line = []
+          for(var i=0; i<size; i++){
+            current_line[i] = {is_active: false};
+          }
+          $scope.matrix[row] = current_line;
+        }
 
         $scope.cell_clicked = function(cell){
           cell.is_active = !cell.is_active;
