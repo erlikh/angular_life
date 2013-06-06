6ee3502cbcf3c3e206c51d66e45cc6bd2f164d0b
Nick Erlikh <n.erlikh@gmail.com>
2013-06-06 17:24:47 +0700

    Add cell toggle

--- life.js
+++ life.js
@@ -1,10 +1,27 @@
+(function() {
+
 angular.module("life", [])
   .directive("game", function factory(){
     return {
       restrict: "AEC",
       replace: true,
-      template: "<div>It works</div>",
-      transclude: true,
-      link: function(){}
+      scope: false,
+      template: "" +
+        "<div ng-repeat='cell in cells'>" +
+          "<div ng-class='{active: cell.is_active}' class='cell' ng-click='cell_clicked(cell)'></div>" +
+        "</div>",
+      transclude: false,
+      link: function(){
+
+      },
+      controller: function($scope){
+        $scope.cells = [{is_active: true}, {is_active: false}]
+
+        $scope.cell_clicked = function(cell){
+          cell.is_active = !cell.is_active;
+        }
+      }
     }
   });
+
+}).call(this);
