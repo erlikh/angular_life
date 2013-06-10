cd1cf63a0145991740d8035959fede4c0fbc3d82
Nick Erlikh <n.erlikh@gmail.com>
2013-06-10 22:52:28 +0700

    Add ability to pass function to seed

--- js/life.js
+++ js/life.js
@@ -1,7 +1,21 @@
 angular.module("life", [])
   .service("PlayField", function(){
+    var _clone = function(obj){
+      var result = {};
+      for(var key in obj){
+        if(obj.hasOwnProperty(key)){
+          var value = obj[key];
+          if(value instanceof Function){
+            value = value();
+          }
+          result[key] = value;
+        }
+      }
+      return result;
+    };
+
     return {
-      count_active_neighbors: function(neighbors, matrix){
+      count_active: function(matrix, neighbors){
         return neighbors.reduce(function(count, neighbor_index){
           var element = matrix[neighbor_index[0]][neighbor_index[1]];
           if(element.is_active){
@@ -11,26 +25,7 @@ angular.module("life", [])
         }, 0);
       },
 
-      map: function(matrix, callback){
-        return matrix.map(function(vector, row){
-          return vector.map(function(element, column){
-            return callback(element, [row, column], matrix);
-          });
-        });
-      },
-
-      seed: function(size){
-        var matrix = [], row, column;
-        for (row = 0; row < size; row++){
-          matrix[row] = [];
-          for (column = 0; column < size; column++){
-            matrix[row][column] = {};
-          }
-        }
-        return matrix;
-      },
-
-      sub: function(matrix, index){
+      get_neighbors: function(matrix, index){
         var result = [], row, column;
         for (row = index[0]-1; row <= index[0]+1; row++){
           if(!matrix[row]){ continue; }
@@ -43,6 +38,25 @@ angular.module("life", [])
           }
         }
         return result;
+      },
+
+      map: function(matrix, callback){
+        return matrix.map(function(vector, row){
+          return vector.map(function(element, column){
+            return callback(element, [row, column], matrix);
+          });
+        });
+      },
+
+      seed: function(size, value){
+        var matrix = [], row, column;
+        for (row = 0; row < size; row++){
+          matrix[row] = [];
+            for (column = 0; column < size; column++){
+            matrix[row][column] = _clone(value);
+          }
+        }
+        return matrix;
       }
     };
   })
@@ -73,15 +87,18 @@ angular.module("life", [])
         "</div>",
       controller: function($scope){
         var make_judgment
-          , is_stopped=true;
+          , is_stopped=true
+          , seeder;
 
         make_judgment = function(element, index, matrix){
-          var neighbors = element.neighbors || Playfield.sub(matrix, index);
-          var active_neighbors_count = Playfield.count_active_neighbors(neighbors, matrix);
-          var is_active = Judge[!!element.is_active][active_neighbors_count];
+          var neighbors = element.neighbors || Playfield.get_neighbors(matrix, index);
+          var active_count = Playfield.count_active(matrix,neighbors);
+          var is_active = !!Judge[element.is_active][active_count];
 
           return {is_active: is_active, neighbors: neighbors};
         };
+
+        seeder = function(){ return Math.random()*10 > 8; };
 
         $scope.cell_clicked = function(cell){
           is_stopped = true;
@@ -90,7 +107,7 @@ angular.module("life", [])
 
         $scope.reset = function(){
           is_stopped = true;
-          $scope.matrix = Playfield.seed($scope.size);
+          $scope.matrix = Playfield.seed($scope.size, {is_active: false});
         };
 
         $scope.start = function(forced){
@@ -105,7 +122,7 @@ angular.module("life", [])
           is_stopped = true;
         };
 
-        $scope.matrix = Playfield.seed($scope.size);
+        $scope.matrix = Playfield.seed($scope.size, {is_active: seeder});
       }
     };
   }]);
